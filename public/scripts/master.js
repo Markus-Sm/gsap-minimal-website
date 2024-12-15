641c3cb3ce378fc6393c90f1902699f8e8d3c92b
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2024-12-15 19:57:48 +0100

    adding gsap to menu and first section

--- public/scripts/master.js
+++ public/scripts/master.js
@@ -19,4 +19,27 @@ const t1_img = gsap
       y: "100%",
     },
     "-=0.7"
-  );
+  )
+  .from(".img img", {
+    opacity: 0,
+    duration: 1,
+    scale: 1.2,
+    stagger: 0.2,
+  })
+  .from(".text h1", {
+    opacity: 0,
+    duration: 1,
+    y: 50,
+  })
+  .from(
+    ".text h5",
+    {
+      opacity: 0,
+      duration: 1,
+      y: 50,
+    },
+    "-=.5"
+  )
+  .to(".content", {
+    y: "80px",
+  });

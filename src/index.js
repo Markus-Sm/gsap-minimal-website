2c479b3a74bbfd14f5c1e8f20eb658ab1c0cca66
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2024-12-15 16:45:40 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./scss/master.scss";
+import gsap from "gsap";
+
+gsap.to(".inscription", {
+  duration: 1,
+  x: 100,
+  y: 100,
+});
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+};

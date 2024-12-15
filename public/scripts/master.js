4d6129a32b54779d6c0b01fd8a4d896c4fca893f
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2024-12-15 20:04:25 +0100

    end of the first section with animations

--- public/scripts/master.js
+++ public/scripts/master.js
@@ -43,3 +43,24 @@ const t1_img = gsap
   .to(".content", {
     y: "80px",
   });
+
+const t1_header = gsap
+  .timeline({ ease: "Power1.easeInOut" })
+  .from(".header", {
+    delay: 4.5,
+    duration: 1,
+    y: -80,
+  })
+  .from(".header .logo", {
+    duration: 1,
+    y: -80,
+  })
+  .from(
+    "header .menu li",
+    {
+      duration: 1,
+      y: -80,
+      stagger: 0.1,
+    },
+    "-=.4"
+  );

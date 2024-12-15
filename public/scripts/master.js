f6df9275d939a8d10f70297aba132aa453408b5f
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2024-12-15 19:33:48 +0100

    adding structure and styles

--- public/scripts/master.js
+++ public/scripts/master.js
@@ -5,3 +5,18 @@ gsap.to(".inscription", {
   x: 100,
   y: 100,
 });
+
+const t1_img = gsap
+  .timeline({ ease: "Power1.easeInOut" })
+  .to(".men .img-overlay", {
+    duration: 1,
+    y: "100%",
+  })
+  .to(
+    ".women .img-overlay",
+    {
+      duration: 1,
+      y: "100%",
+    },
+    "-=0.7"
+  );
